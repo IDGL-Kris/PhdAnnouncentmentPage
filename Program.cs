c68fd4844c58ea1581a46272ebe0ab630cd70c73
IDGL-Kris <kristianvberlo@gmail.com>
2022-07-29 21:15:20 +0200

    Fixed Some layout

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
+using MatBlazor;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using RomanaPhdPage;
@@ -5,5 +6,6 @@ using RomanaPhdPage;
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+builder.Services.AddMatBlazor();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 await builder.Build().RunAsync();
